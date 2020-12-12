3cc3b508ee10bfde8b191adf544913241ca29a2d
charliebrown364 <364charliebrown@gmail.com>
2020-12-12 09:44:45 +0000

    Finished Assignment 40-2. In this assignment, I updated my EulerEstimator() class to work for any dimension of functions.

--- src/euler_estimator.py
+++ src/euler_estimator.py
@@ -1,20 +1,18 @@
-import matplotlib.pyplot as plt
-plt.style.use('bmh')
+# import matplotlib.pyplot as plt
+# plt.style.use('bmh')
 
 class EulerEstimator():
 
-    def __init__(self, derivative):
-        self.derivative = derivative
+    def __init__(self, derivatives):
+        self.derivatives = derivatives
     
     def calc_derivative_at_point(self, point):
-        x = point[0]
-        return self.derivative(x)
+        y_init_values = list(point[1].values())
+        return {key:value(y_init_values, point[1]) for (key,value) in self.derivatives.items()}
 
     def step_forward(self, point, step_size):
-        x = point[0]
-        y = point[1]
-        new_x = x + step_size
-        new_y = y + (self.calc_derivative_at_point(point) * step_size)
+        new_x = point[0] + step_size
+        new_y = {key:point[1][key] + step_size*value for key, value in self.calc_derivative_at_point(point).items()}
         return (new_x, new_y)
 
     def calc_estimated_points(self, point, step_size, num_steps):
@@ -26,6 +24,7 @@ class EulerEstimator():
     
     def plot(self, point, step_size, num_steps):
         
+        """
         x_coords = [round(pair[0], 4) for pair in self.calc_estimated_points(point, step_size, num_steps)]
         y_coords = [round(pair[1], 4) for pair in self.calc_estimated_points(point, step_size, num_steps)]
         
@@ -34,3 +33,4 @@ class EulerEstimator():
         plt.ylabel('y')
         plt.title('Euler estimation')
         plt.savefig('euler_estimation.png')
+        """
--- tests/test_euler_estimator.py
+++ tests/test_euler_estimator.py
@@ -2,24 +2,38 @@ import sys
 sys.path.append('src')
 from euler_estimator import EulerEstimator
 
-print("Testing Assignment 24-1...")
+def rounding(this_dict):
+    return {key:round(value, 5) for key, value in this_dict.items()}
 
-euler = EulerEstimator(lambda t: t+1)
+derivatives = {
+                'A': (lambda t,x: x['A'] + 1),
+                'B': (lambda t,x: x['A'] + x['B']),
+                'C': (lambda t,x: 2*x['B']) 
+              }
 
-assert euler.calc_derivative_at_point((1, 4)) == 2, euler.calc_derivative_at_point((1, 4))
+euler = EulerEstimator(derivatives = derivatives)
 
-assert euler.step_forward((1, 4), 0.5) == (1.5, 5), euler.step_forward((1, 4), 0.5)
+initial_values = {'A': 0, 'B': 0, 'C': 0}
+initial_point = (0, initial_values)
+
+assert euler.calc_derivative_at_point(initial_point) == {'A': 1, 'B': 0, 'C': 0}, euler.calc_derivative_at_point(initial_point)
+
+point_2 = euler.step_forward(point = initial_point, step_size = 0.1)
+assert point_2 == (0.1, {'A': 0.1, 'B': 0, 'C': 0}), point_2
+
+assert euler.calc_derivative_at_point(point_2) == {'A': 1.1, 'B': 0.1, 'C': 0}
+
+point_3 = euler.step_forward(point = point_2, step_size = -0.5)
+assert (point_3[0], rounding(point_3[1])) == (-0.4, {'A': -0.45, 'B': -0.05, 'C': 0}), point_3
 
 ans = [
-        (1, 4), # starting point
-        (1.5, 5), # after 1st step
-        (2, 6.25), # after 2nd step
-        (2.5, 7.75), # after 3rd step
-        (3, 9.5) # after 4th step
+        (-0.4, {'A': -0.45, 'B': -0.05, 'C': 0}), # starting point 
+        (1.6, {'A': 0.65, 'B': -1.05, 'C': -0.2}), # after 1st step
+        (3.6, {'A': 3.95, 'B': -1.85, 'C': -4.4}), # after 2nd step
+        (5.6, {'A': 13.85, 'B': 2.35, 'C': -11.8}) # after 3rd step
       ]
 
-assert euler.calc_estimated_points((1, 4), 0.5, 4) == ans, euler.calc_estimated_points((1, 4), 0.5, 4)
+problem = [(pair[0], rounding(pair[1])) for pair in euler.calc_estimated_points(point=point_3, step_size=2, num_steps=3)]
+assert problem == ans, problem
 
-euler.plot((-5, 10), 0.1, 100)
-
-print("Passed Assignment 24-1")
+print("passed all")
